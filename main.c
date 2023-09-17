4745543f5a3cc032e96b4161fe5180c5e83a5a34
mojo <mojo@localhost.localdomain>
2023-09-17 21:14:15 +0800

    .

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 void RunC100() {
     _5_3_QuickSort();
+    _5_4_StableQuickSort();
     _8_1_MagicSquareOdd(5);
     _8_5_EightQueen();
     _8_6_KnightTour(5, 5);    
@@ -17,9 +18,9 @@ void RunUt() {
     Ut();
 }
 
-int main() {
-
-    //RunC100();
+int main() {    
+    //_8_8_EulerTrial();
+    RunC100();
     //Ut();
 
     printf("main() ended\n");
--- util/Util.h
+++ util/Util.h
@@ -3,3 +3,14 @@
 #include "assert.h"
 
 void check(int ret);
+
+typedef struct Pair {
+    void* first;
+    void* second;
+} Pair;
+
+typedef struct Triple {
+    void* first;
+    void* second;
+    void* third;
+} Triple;
